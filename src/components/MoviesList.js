9850b7c967053435302e318cc645ad3b03e326a3
imrajashish <imrajashish07@gmail.com>
2025-01-09 03:11:49 +0530

    MovieList and Moviecard

---
+++ src/components/MoviesList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import MoviesCard from "./MoviesCard";
+
+function MoviesList({ title, movies }) {
+  console.log("movies", movies);
+
+  return (
+    <div>
+      <div className="p-6 ">
+        <h1 className="text-4xl py-4 text-white">{title}</h1>
+        <div className="flex overflow-x-scroll">
+          <div className="flex">
+            {movies?.map((movie) => (
+              <MoviesCard key={movie.id} posterPath={movie.poster_path} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MoviesList;
--- src/components/SeconderyContainer.js
+++ src/components/SeconderyContainer.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import MoviesList from "./MoviesList";
+
+import { useSelector } from "react-redux";
 
 function SeconderyContainer() {
+  const movies = useSelector((store) => store.movies);
   return (
-    <div>SeconderyContainer</div>
-  )
+    movies.nowPlayingMovies && (
+      <div className=" bg-black">
+        <div className="-mt-52 relative z-20">
+          <MoviesList
+            title={"Now playing Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+          <MoviesList
+            title={"Trending Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+          <MoviesList
+            title={"Popular Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+          <MoviesList
+            title={"UpComing Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+          <MoviesList
+            title={"Horror Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+        </div>
+      </div>
+    )
+  );
 }
 
-export default SeconderyContainer
+export default SeconderyContainer;
